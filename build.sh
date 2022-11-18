15d55b295d2ffa169ba2c036755c92b641730aa7
Kilian Northoff <kilian@northoff-online.de>
2022-11-18 11:15:00 +0100

    fix

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-cargo build --release
+cargo build --target x86_64-unknown-linux-musl --release
 cargo build --release --target=x86_64-pc-windows-gnu
 
 mkdir -p bin
 
-cp target/release/edit_config bin
+cp target/x86_64-unknown-linux-musl/release/edit_config bin
 cp target/x86_64-pc-windows-gnu/release/edit_config.exe bin
 
 chmod +x bin/*
